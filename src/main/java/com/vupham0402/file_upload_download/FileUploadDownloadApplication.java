faedd0bad597f8e3b0b1d65df80a0995cb246a7f
Ken Pham <kpham@eyeprogpo.com>
2023-02-15 19:54:03 -0500

    First commit

---
+++ src/main/java/com/vupham0402/file_upload_download/FileUploadDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.vupham0402.file_upload_download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadDownloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vupham0402/file_upload_download/FileUploadDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vupham0402.file_upload_download;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
